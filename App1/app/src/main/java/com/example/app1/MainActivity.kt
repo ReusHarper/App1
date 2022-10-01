fc5606770764cc416bb7dd1ec9919aad9cbc457e
ReusHarper <reusharper@comunidad.unam.mx>
2022-10-01 09:25:01 -0500

    Cambio de ubicacion de archivos

--- App1/app/src/main/java/com/example/app1/MainActivity.kt
+++ App1/app/src/main/java/com/example/app1/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.app1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val a = 5
+        val b = 20
+        val c = a * b
+
+        Log.d("INFORMATION", "Este es un mensaje de informacion")
+        Log.d("INFORMATION", "La multiplicacion de axb es: " + c)
     }
 }
